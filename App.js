0b8c2a046a63923353c1f5e2c398a4bbb58e7fb4
lofti198 <isolar2005@gmail.com>
2022-08-08 19:54:12 +0300

    stylesheet topic

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Linking } from "react-native";
+import React, { useState } from "react";
 export default function App() {
+  const [text, setText] = useState("Mash");
+  const [session, setSession] = useState({ number: 6, title: "name" });
+  const [current, setCurrent] = useState(true);
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        {text} {session.title} {current ? "cur" : "prev"} {counter}
+      </Text>
+      <View style={styles.button}>
+        <Button
+          title="Yotubue channel"
+          onPress={() => {
+            // Linking.openURL("https://youtube.com");
+            setText("hji");
+            setSession({ ...session, title: "newname" });
+            setCurrent((prev) => !prev);
+            setCounter((prev) => prev + 5);
+          }}
+        ></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +30,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "50%",
+    // flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 10,
+    borderColor: "#ff00ff",
+    borderRadius: 10,
+    // margin: 40,
+  },
+  text: {
+    color: "red",
+    fontSize: 20,
+    margin: 10,
+    textTransform: "uppercase",
+  },
+  button: {
+    width: 100,
   },
 });
