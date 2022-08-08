d0d3c09213ee54559e5015a88c16f8b6e4321370
lofti198 <isolar2005@gmail.com>
2022-08-08 20:17:20 +0300

    flex lesson before hometask

--- App.js
+++ App.js
@@ -7,47 +7,63 @@ export default function App() {
   const [current, setCurrent] = useState(true);
   const [counter, setCounter] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        {text} {session.title} {current ? "cur" : "prev"} {counter}
-      </Text>
-      <View style={styles.button}>
-        <Button
-          title="Yotubue channel"
-          onPress={() => {
-            // Linking.openURL("https://youtube.com");
-            setText("hji");
-            setSession({ ...session, title: "newname" });
-            setCurrent((prev) => !prev);
-            setCounter((prev) => prev + 5);
-          }}
-        ></Button>
+    <View style={styles.body}>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
       </View>
-      <StatusBar style="auto" />
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "50%",
-    // flex: 1,
-    backgroundColor: "#fff",
+  body: {
+    // width: "100%",
+    // height: "50%",
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#0000ff",
+    alignItems: "stretch",
+    justifyContent: "center",
+    // borderWidth: 10,
+    // borderColor: "#ff00ff",
+    // borderRadius: 10,
+    // margin: 40,
+  },
+  view1: {
+    flex: 3,
+    // width: 100,
+    // height: 100,
+    backgroundColor: "green",
     alignItems: "center",
     justifyContent: "center",
-    borderWidth: 10,
-    borderColor: "#ff00ff",
-    borderRadius: 10,
-    // margin: 40,
+  },
+  view2: {
+    flex: 1,
+    // width: 100,
+    // height: 100,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  view3: {
+    flex: 1,
+    // width: 100,
+    // height: 100,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     color: "red",
     fontSize: 20,
     margin: 10,
     textTransform: "uppercase",
-  },
-  button: {
-    width: 100,
   },
 });
